f0475fcbe4cccfd6e2272a7f1f5beb84c6ae15d8
Leonfal <leon2.fallman@gmail.com>
2022-09-28 14:49:57 +0200

    Added code for task 2

--- dtek-lab3/time4timer/mipslabwork.c
+++ dtek-lab3/time4timer/mipslabwork.c
@@ -17,6 +17,7 @@
 int mytime = 0x5957;
 
 char textstring[] = "text, more text, and even more text!";
+int timeoutcounter = 0;
 
 // volatile pointer to point to adress where PORTE is.
 volatile int *porte = (volatile int*)0xbf886110;
@@ -41,20 +42,42 @@ void labinit( void )
 
   // Stop timer and clear control register set prescaler at 1:1
   T2CON = 0x0;
-
   // clear timer register
   TMR2 = 0x0;
   // load period register
-  PR2 = 0xFFFF;
-  // start timer
-  T2CONSET = 0x8000;
-
+  PR2 = 0x7a12;
+  // 80 mhz / 256 = 312 500
+  // wants 10 times per second = 31250 = 0x7a12
+  // because 312500/31250 = 10 it will display 10 times / second.
+  // T2CONSET = 0x8070; // set prescaler to 1:256 and start timer
+  // bit 15 = if timer is on, bit 4-6 if all is on, prescaler to 256.
+  T2CONSET = 0x8070;
   return;
 }
 
 /* This function is called repetitively from the main program */
 void labwork( void )
 {
+  // if flag is 1, reset event.
+  // Timer 2 interrupt flag. Can be found at bit 8 in IFS0
+
+  if((IFS(0) & 0x100))
+  {
+    // reset flag
+    IFSCLR(0) = 0x100;
+    timeoutcounter++;
+    if ((timeoutcounter % 10) == 0)
+    {
+      time2string(textstring, mytime);
+      display_string( 3, textstring );
+      display_update();
+      tick( &mytime );
+      // increment the deferenced pointer by one bit each time tick is called.
+      *porte += 0x1;
+      display_image(96, icon);
+    }
+  }
+
   int btn = getbtns();
   if(btn != 0x0)
   {
@@ -89,14 +112,6 @@ void labwork( void )
     }
 
   }
-  delay(1000);
-  time2string( textstring, mytime );
-  display_string( 3, textstring );
-  display_update();
-  tick( &mytime );
-  // increment the deferenced pointer by one bit each time tick is called.
-  *porte += 0x1;
-  display_image(96, icon);
-
+  // delay(1000);
 
 }
